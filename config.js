c9adaf72bfe68d859c653b7bccff0c40d2a4a03f
Andreia-Irina Ocanoaia <andreia@genez.io>
2024-10-21 20:12:12 +0000

    Added log

--- config.js
+++ config.js
@@ -4,16 +4,17 @@ require('dotenv').config();
 const mongoURI = process.env.MY_MONGO_DB_DATABASE_URL;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            ssl: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err);
-        process.exit(1);
-    }
+  try {
+    console.log('test');
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      ssl: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
